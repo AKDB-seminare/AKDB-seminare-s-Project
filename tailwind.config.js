eabd4289a9c8433f275b1344e2ffc4a816cf6be5
AKDB-seminare <dohnal@dohnalbernard.cz>
2025-02-13 08:14:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        DEFAULT: 'hsl(var(--border) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 }
