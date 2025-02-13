8926005b78768c57fcf068add42a2adf43a41469
AKDB-seminare <dohnal@dohnalbernard.cz>
2025-02-13 07:35:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+//** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_SUPABASE_URL: 'https://cltlakihutuggzzrmvqm.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsdGxha2lodXR1Z2d6enJtdnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNjQxMzAsImV4cCI6MjA1NDk0MDEzMH0.AQ8q52Vxh4ILQX9fMLgc_QsPbzwROIvlg94qoTAPbuI'
+  },
+  experimental: {
+    css: true
   }
 }
-
 module.exports = nextConfig
